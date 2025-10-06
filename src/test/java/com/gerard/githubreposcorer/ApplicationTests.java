67a6e35b498e70b897a199f09d426f157e88061b
slatmother <slatmother@gmail.com>
2025-10-06 22:15:09 +0300

    application and test

---
+++ src/test/java/com/gerard/githubreposcorer/ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gerard.githubreposcorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+
+}
